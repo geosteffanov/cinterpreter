cd4f2d278b609abc15f6488f48b348f105a5dd5d
Georgi Stefanov <georgi.stefanov@uber.com>
2018-11-27 09:36:43 +0200

    Add exit func

--- shell.c
+++ shell.c
@@ -8,7 +8,12 @@
 */
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 # define BUFFER_SIZE 1024
+# define EXIT_CMD 1
+# define NOT_FOUND 0
 
 char buffer[BUFFER_SIZE];
 
@@ -20,12 +25,35 @@ int read_input() {
 }
 
 
+void write_prompt() {
+	// printf ??
+	write(0, ">>>", 3);
+}
+
+int parse_input(int bytes_read) {
+	int cmpresult = strncmp("exit\n", buffer, bytes_read);
+	if (cmpresult == 0) {
+		return EXIT_CMD;
+	}
+
+	return NOT_FOUND;
+}
+
+
 
 int main() {
 	while(1) {
+		write_prompt();
+
 		int bytes_read = read_input();
 
-		printf("read input %s", buffer);
+		printf("read input: %s", buffer);
+
+		if (parse_input(bytes_read) == EXIT_CMD) {
+			exit(0);
+		}
+
+
 	}
 
 	return 0;
