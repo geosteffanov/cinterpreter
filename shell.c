ba5ec90ab1dffc437f74c730a651d6e107ab7a69
Georgi Stefanov <georgi.stefanov@uber.com>
2018-11-27 10:21:45 +0200

    Add strip_chars

--- shell.c
+++ shell.c
@@ -13,10 +13,10 @@
 
 # define BUFFER_SIZE 1024
 # define EXIT_CMD 1
-# define NOT_FOUND 0
+# define COMMAND 2
 
 char buffer[BUFFER_SIZE];
-
+char clean_input[BUFFER_SIZE];
 
 int read_input() {
 	int bytes_read = read(1, buffer, BUFFER_SIZE);
@@ -24,36 +24,68 @@ int read_input() {
 	return bytes_read;
 }
 
+void strip_chars(char* source) {
+	int idx_source = 0;
+	int idx_result = 0;
+	int length_source = strlen(source);
+
+	while (idx_source < length_source) {
+		char at_index = source[idx_source];
+		if (at_index == ' ' || at_index == '\t' || at_index == '\n') {
+		    source[idx_result] = at_index;
+		    idx_source++;
+		    idx_result++;
+		}
+		while (at_index == ' ' || at_index == '\t' || at_index == '\n') {
+			idx_source++;
+			at_index = source[idx_source];
+		}
+
+		source[idx_result] = at_index;
+		idx_source++;
+		idx_result++;
+	}
+
+	source[idx_result] = 0;
+}
+
+/*
+	STATE 1: While reading \t, \n \s skip ahead.
+	STATE 2:
+*/
+
 
 void write_prompt() {
 	// printf ??
 	write(0, ">>>", 3);
 }
 
-int parse_input(int bytes_read) {
-	int cmpresult = strncmp("exit\n", buffer, bytes_read);
+int parse_input(char* input, int length) {
+	int cmpresult = strncmp("exit", input, length);
 	if (cmpresult == 0) {
 		return EXIT_CMD;
 	}
 
-	return NOT_FOUND;
+	return COMMAND;
 }
-
-
 
 int main() {
 	while(1) {
 		write_prompt();
 
 		int bytes_read = read_input();
-
 		printf("read input: %s", buffer);
 
-		if (parse_input(bytes_read) == EXIT_CMD) {
+
+		strncpy(clean_input, buffer, bytes_read);
+		printf("copied: %s\n", clean_input);
+		strip_chars(clean_input);
+		printf("After stripping: %s\n", clean_input);
+
+		int input_type = parse_input(clean_input, strlen(clean_input));
+		if (input_type == EXIT_CMD) {
 			exit(0);
 		}
-
-
 	}
 
 	return 0;
